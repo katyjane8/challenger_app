2c229bb3cbf57473901a9e9cc1001fb0c94d946e
katyjane8 <katy.jane8@gmail.com>
2017-12-13 15:23:14 -0700

    delete working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :destroy, :edit, :update]
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   def index
     @students = Student.all
@@ -25,14 +25,27 @@ class StudentsController < ApplicationController
   def edit
   end
 
-  private
+  def update
+    @student.update(student_params)
 
-  def student_params
-    params.require(:student).permit(:name)
+    redirect_to student_path(@student)
   end
 
-  def set_student
-    @student = Student.find(params[:id])
+  def destroy
+    @student
+    @student.destroy
+
+    redirect_to students_path
   end
+
+    private
+
+    def student_params
+      params.require(:student).permit(:name)
+    end
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 
 end
